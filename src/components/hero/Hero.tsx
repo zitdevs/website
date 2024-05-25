55b70c8b9ea756eda563490f5729e0cda9b238ca
Isaac Martinez <isaacismaelx14@gmail.com>
2024-05-24 23:51:54 -0400

    style(hero): adjust spacing and responsiveness in Hero component
    - Adjusted vertical spacing in Hero section for better layout
    - Modified font sizes for different screen sizes in hero title
    - Updated button padding for better responsiveness

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -7,11 +7,11 @@ export type HeroProps = {
 
 const Hero: React.FC<HeroProps> = ({ tHero }) => {
   return (
-    <section className="w-full flex mt-32 flex-col items-center space-y-10">
-      <h2 className="font-semibold text-4xl sm:text-5xl md:text-6xl max-w-[42rem] text-center text-pretty">
+    <section className="w-full flex mt-32 flex-col items-center space-y-5 md:space-y-10">
+      <h2 className="font-semibold text-3xl sm:text-5xl md:text-6xl w-full md:max-w-[42rem] text-center text-pretty">
         {tHero.title}
       </h2>
-      <Button className="px-12 py-6 font-bold text-xs sm:text-sm">
+      <Button className="px-10 py-6 md:px-12 font-bold text-xs sm:text-sm">
         {tHero.cta}
       </Button>
     </section>
