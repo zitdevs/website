eb04a0fca35ce76d0587b19416671c3f11b3fd22
Isaac Martinez <isaacismaelx14@gmail.com>
2024-05-24 22:41:14 -0400

    feat(hero): update hero section text and styling
    - Updated hero title and CTA text in English and Spanish translations
    - Modified Hero component to apply new styles to the title

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -8,7 +8,7 @@ export type HeroProps = {
 const Hero: React.FC<HeroProps> = ({ tHero }) => {
   return (
     <section className="w-full flex mt-32 flex-col items-center space-y-10">
-      <h2 className="font-semibold text-6xl max-w-2xl text-center">
+      <h2 className="font-semibold text-6xl max-w-[42rem] text-center text-pretty">
         {tHero.title}
       </h2>
       <Button className="px-12 py-6 font-bold">{tHero.cta}</Button>
