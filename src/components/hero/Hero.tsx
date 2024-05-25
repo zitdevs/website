158ee78478681cf82bbede31aaf2baedb3f34791
Isaac Martinez <isaacismaelx14@gmail.com>
2024-05-24 23:20:53 -0400

    feat(hero): improve responsiveness of hero section
    - Adjusted font size of hero title for different screen sizes
    - Updated button text size to be responsive

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -8,10 +8,12 @@ export type HeroProps = {
 const Hero: React.FC<HeroProps> = ({ tHero }) => {
   return (
     <section className="w-full flex mt-32 flex-col items-center space-y-10">
-      <h2 className="font-semibold text-6xl max-w-[42rem] text-center text-pretty">
+      <h2 className="font-semibold text-4xl sm:text-5xl md:text-6xl max-w-[42rem] text-center text-pretty">
         {tHero.title}
       </h2>
-      <Button className="px-12 py-6 font-bold">{tHero.cta}</Button>
+      <Button className="px-12 py-6 font-bold text-xs sm:text-sm">
+        {tHero.cta}
+      </Button>
     </section>
   );
 };
