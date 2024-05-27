8bfc616e56b82c7f0f9850230292e2c0aad943ae
Isaac Martinez <isaacismaelx14@gmail.com>
2024-05-26 21:04:55 -0400

    refactor: integrate react-hook-form with zod validation in ZitlancerForm
    - Replace local state management with react-hook-form
    - Add zod schema for form validation
    - Update form fields to use Form, FormField, FormControl, FormLabel, and FormMessage components
    - Ensure otherSkill field appears only when mainSkill is 'other' and is validated accordingly

--- src/modules/conctact/components/zitlancer-form.tsx
+++ src/modules/conctact/components/zitlancer-form.tsx
@@ -6,80 +6,119 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/select/Select";
-import { useEffect, useState } from "react";
-import { ZitLancerStoreState, useContactStore } from "../store/contact.store";
+import { useEffect } from "react";
+import { useContactStore } from "../store/contact.store";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  FormDescription,
+} from "@/components/form/Form";
 
 const ZitLancerForm = () => {
-  const setZitLancerData = useContactStore((state) => state.setZitLancerData);
-  const isSubmitting = useContactStore((state) => state.isSubmitting);
-  const [data, setData] = useState<ZitLancerStoreState>({
-    mainSkill: "",
-    otherSkill: "",
-    country: "",
+  const schema = z.object({
+    mainSkill: z.string().min(0, "Main skill is required"),
+    otherSkill: z.string().optional(),
   });
 
+  type ValidationSchemaType = z.infer<typeof schema>;
+
+  const form = useForm<ValidationSchemaType>({ resolver: zodResolver(schema) });
+  const isSubmitting = useContactStore((state) => state.isSubmitting);
+
   useEffect(() => {
-    if (isSubmitting) setZitLancerData(data);
-  }, [data, isSubmitting, setZitLancerData]);
+    if (isSubmitting) {
+      form.handleSubmit((e) => {
+        if (e.mainSkill === "other" && !e.otherSkill) {
+          form.setError("otherSkill", {
+            type: "manual",
+            message: "Other skill is required",
+          });
+          return;
+        }
+
+        console.log(e);
+      })();
+    }
+  }, [form, isSubmitting]);
 
   return (
-    <>
-      <div className="sm:col-span-2">
-        <label
-          htmlFor="main-skill"
-          className="block text-sm font-semibold leading-6 text-gray-900"
-        >
-          Main Skill <span className="text-red-500">*</span>
-        </label>
-        <div className="mt-2.5">
-          <Select
-            onValueChange={(value) =>
-              setData((prev) => ({ ...prev, mainSkill: value }))
-            }
-            value={data.mainSkill}
-          >
-            <SelectTrigger className="w-full" id="main-skill">
-              <SelectValue placeholder="Select your main skill" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="software-dev">Software Development</SelectItem>
-              <SelectItem value="web-dev">Web Development</SelectItem>
-              <SelectItem value="mobile-dev">Mobile Development</SelectItem>
-              <SelectItem value="design">Design</SelectItem>
-              <SelectItem value="marketing">Marketing</SelectItem>
-              <SelectItem value="data-science">Data Science</SelectItem>
-              <SelectItem value="other">Other</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
-
-      {data.mainSkill === "other" && (
+    <Form {...form}>
+      <form className="col-span-2 grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
         <div className="sm:col-span-2">
-          <label
-            htmlFor="other-skill"
-            className="block text-sm font-semibold leading-6 text-gray-900"
-          >
-            Other Skill <span className="text-red-500">*</span>
-          </label>
-          <div className="mt-2.5">
-            <input
-              type="text"
-              name="other-skill"
-              id="other-skill"
-              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-secondary sm:text-sm sm:leading-6"
-              onChange={(e) =>
-                setData((prev) => ({ ...prev, otherSkill: e.target.value }))
-              }
-              value={data.otherSkill}
-            />
-            <span className="text-xs text-gray-400">
-              Please specify your main skill
-            </span>
-          </div>
+          <div className="mt-2.5"></div>
+          <FormField
+            control={form.control}
+            name="mainSkill"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  Main Skill <span className="text-red-500">*</span>
+                </FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Select your main skill" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="software-dev">
+                      Software Development
+                    </SelectItem>
+                    <SelectItem value="web-dev">Web Development</SelectItem>
+                    <SelectItem value="mobile-dev">
+                      Mobile Development
+                    </SelectItem>
+                    <SelectItem value="design">Design</SelectItem>
+                    <SelectItem value="marketing">Marketing</SelectItem>
+                    <SelectItem value="data-science">Data Science</SelectItem>
+                    <SelectItem value="other">Other</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
-      )}
-    </>
+
+        {form.watch("mainSkill") === "other" && (
+          <div className="sm:col-span-2">
+            <FormField
+              control={form.control}
+              name="otherSkill"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Other Skill <span className="text-red-500">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <input
+                      type="text"
+                      autoComplete="off"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-secondary sm:text-sm sm:leading-6"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Please specify your main skill
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        )}
+      </form>
+    </Form>
   );
 };
 
