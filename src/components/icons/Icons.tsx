f6877088506e5162856ff19a9ce9f7bfe5725859
José Tomás Genao <josetgenao26@gmail.com>
2024-08-18 19:33:25 -0400

    feat: WEB-3 add how i works section to astro

---
+++ src/components/icons/Icons.tsx
@@ -0,0 +1,15 @@
+import { MessageSquareMore, Calendar, ScanEye, Check, HandCoins, UserRoundCog, SearchCode, CalendarClock, CircleDollarSign } from 'lucide-react';
+
+const icons = {
+    "message-square-more": MessageSquareMore,
+    "calendar": Calendar,
+    "scan-eye": ScanEye,
+    "check": Check,
+    "hand-coins": HandCoins,
+    "user-round-cog": UserRoundCog,
+    "search-code": SearchCode,
+    "calendar-clock": CalendarClock,
+    "circle-dollar-sign": CircleDollarSign
+}
+
+export default icons;
