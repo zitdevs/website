406ddafe7e71c8160dbdeffe3656e132a2791f98
Isaac Martinez <isaacismaelx14@gmail.com>
2024-08-26 07:32:46 -0400

    chore(astro): update server configuration to listen on all network interfaces
    - Updated 'astro.config.mjs' to configure the server to listen on '0.0.0.0', allowing access from all network interfaces.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: '0.0.0.0'
+  },
   output: "server",
   adapter: node({
     mode: "standalone",
