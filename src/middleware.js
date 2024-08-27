c7d5691ab5b4e16af9d8430f0ac3e5050674c490
Isaac Martinez <isaacismaelx14@gmail.com>
2024-08-27 15:03:51 -0400

    fix(web): WEB-10 Correct navigation links and middleware handling
    - Updated NavItems.astro to fix relative links to ensure proper routing.
    - Simplified NavLink.astro by removing unnecessary locale logic.
    - Improved middleware.js to handle locale and routing more effectively.

--- src/middleware.js
+++ src/middleware.js
@@ -5,27 +5,34 @@ import { requestHasLocale } from "astro:i18n";
 export const onRequest = defineMiddleware((context, next) => {
   const pathNotFound = notFound(context);
 
-  if (context.url.pathname.match(`/((?!api|_next/static|_next/image|favicon.ico|assets|images|legal).*)`)) return next();
+  if (context.url.pathname === "/") return next();
 
-  if (context.url.pathname === '/') return next();
-  if (context.url.pathname === '/robots.txt') return next();
-  if (context.url.pathname === '/legal/privacy-policy') return next();
-
-  if (context.url.pathname.startsWith('/sitemap')) {
-    if (requestHasLocale(context)) {
-      return next();
-    }
-
+  if (context.url.pathname.match(`/((api|_astro/static|_astro/image|favicon.ico|assets|robots.txt|images|legal).*)`)) {
+    console.log('found');
     return next();
   }
 
-  if (pathNotFound) return pathNotFound;
+  if (context.url.pathname.startsWith('/sitemap')) return next();
+  if (requestHasLocale(context)) return next();
 
+  if (pathNotFound) {
+    const isProduction = process.env.NODE_ENV === "production";
 
-  if (requestHasLocale(context)) {
-    return next();
+    const headerLang = context.request.headers.get("accept-language");
+    const host = context.request.headers.get("host");
+    const protocol = context.request.headers.get("x-forwarded-proto") || isProduction ? "https" : "http";
+
+    const lang = headerLang?.split(",")[0].split("-")[0] || "en";
+    const redirect = `/${lang}${context.url.pathname}`
+
+    return new Response(null, {
+      status: 301,
+      headers: {
+        Location: `${protocol}://${host}${redirect}`,
+      },
+    });
   }
+
 
   return new Response("Not found", { status: 404 });
-
 });
