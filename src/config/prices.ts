1e884de213cf2d072a7b4611be5540b083256626
Isaac Martinez <isaacismaelx14@gmail.com>
2025-02-27 16:31:31 -0400

    Feature/WEB-11 (#17)
    * feat(config): WEB-11 add price configuration and integrate into web development page
    
    * fix(config): WEB-11 update web package pricing to include fees

---
+++ src/config/prices.ts
@@ -0,0 +1,20 @@
+export const PriceConfig = {
+  webDev: {
+    essentials: {
+      price: 439.57,
+      monthly: 36.49,
+    },
+    growth: {
+      price: 962.2,
+      monthly: 52.93,
+    },
+    professional: {
+      price: 2323.46,
+      monthly: 85.1,
+    },
+    flex: {
+      price: 24.99,
+      monthly: 42.56,
+    },
+  },
+};
